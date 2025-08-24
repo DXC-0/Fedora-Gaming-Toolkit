91d5a3726c77cc55e0cee14050431f2ad7a77ddb
DXC-0 <62696671+DXC-0@users.noreply.github.com>
2025-08-24 18:26:27 +0000

    Update script.sh

--- script.sh
+++ script.sh
@@ -56,7 +56,7 @@ install_gaming_tools() {
   TMP_DIR="/tmp/heroic-install"
   mkdir -p "$TMP_DIR"
   cd "$TMP_DIR"
-  RPM_URL="https://github.com/Heroic-Games-Launcher/HeroicGamesLauncher/releases/download/v2.18.1/heroic-2.18.1.rpm"
+  RPM_URL="https://sourceforge.net/projects/heroic-games-launcher.mirror/files/v2.18.1/Heroic-2.18.1-linux-x86_64.rpm/download"
   wget "$RPM_URL" -O heroic.rpm
   dnf install -y ./heroic.rpm
   rm -rf "$TMP_DIR"
